5af68c6d33b4757b2065bdc5399a642c5a73a69b
BrunoQueiroz156 <burnout156@hotmail.com>
2015-05-12 10:19:42 -0300

    Update Motocicleta.java

--- LojaVeiculos/src/entidade/Motocicleta.java
+++ LojaVeiculos/src/entidade/Motocicleta.java
@@ -5,13 +5,8 @@ import objetoDeValor.MontadoraMoto;
 import objetoDeValor.TipoMoto;
 
 public class Motocicleta extends Veiculo{
-
-    private String chassi;
-    private String modelo;
     private int cilindrada;
     private int capacidadeTanque;
-    private float preco;
-    private Cor cor;
     private TipoMoto tipo;
     private MontadoraMoto montadora;
 
@@ -29,22 +24,6 @@ public class Motocicleta extends Veiculo{
         this.montadora = montadora;
     }
 
-    public String getChassi() {
-        return chassi;
-    }
-
-    public void setChassi(String chassi) {
-        this.chassi = chassi;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
     public int getCilindrada() {
         return cilindrada;
     }
@@ -59,22 +38,6 @@ public class Motocicleta extends Veiculo{
 
     public void setCapacidadeTanque(int capacidadeTanque) {
         this.capacidadeTanque = capacidadeTanque;
-    }
-
-    public float getPreco() {
-        return preco;
-    }
-
-    public void setPreco(float preco) {
-        this.preco = preco;
-    }
-
-    public Cor getCor() {
-        return cor;
-    }
-
-    public void setCor(Cor cor) {
-        this.cor = cor;
     }
 
     public TipoMoto getTipo() {
