41c17adff721d1772fc19b8df90b89d7d3396615
BrunoQueiroz156 <burnout156@hotmail.com>
2015-05-12 10:05:38 -0300

    Create Veiculo.java

---
+++ LojaVeiculos/src/entidade/Veiculo.java
@@ -0,0 +1,37 @@
+package entidade;
+import objetoDeValor.Cor;
+
+public abstract class Veiculo {
+	
+	   String chassi;
+	   String modelo;
+	   Cor cor;
+	   float preço;
+	
+	
+   public String getChassi() {
+		return chassi;
+	}
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Cor getCor() {
+		return cor;
+	}
+	public void setCor(Cor cor) {
+		this.cor = cor;
+	}
+	public float getPreço() {
+		return preço;
+	}
+	public void setPreço(float preço) {
+		this.preço = preço;
+	}
+	
+}
