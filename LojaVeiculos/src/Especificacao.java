b5a61bb58ba124c2d47021f8a20f490080c7b050
BrunoQueiroz156 <burnout156@hotmail.com>
2015-06-09 00:53:28 -0300

    Create Especificacao.java

---
+++ LojaVeiculos/src/Especificacao.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Especificacao {
+	{
+
+		ArrayList<String> Cambio = new ArrayList<>();
+		ArrayList<String> Cor = new ArrayList<>();
+		ArrayList<String> tipoMotocicleta = new ArrayList<>();
+		ArrayList<String> montadoraMotocicleta = new ArrayList<>();
+		ArrayList<String> montadoraCarro = new ArrayList<>();
+		ArrayList<String> tipoCarro = new ArrayList<>();
+		String[] montMotocicleta = { "BMW", "HONDA", "BASHAN", "DAFRA",
+				"DAJIANG", "DAYANG", "FLASH", "HARLEY_DAVIDSON", "IROS",
+				"ITALIKA", "KASINSKI", "KAWASAKI", "LONCIN", "SHINERAY",
+				"SUNDOWN", "SUZUKI", "TRAXX", "TRIHMPH", "WUYANG", "YAMAHA" };
+		String[] tipoMoto = { "SCRAMBLER", "TRACKER", "BOBBER", "CAFE_RACER",
+				"CUSTOM", "CHOPPER", "ESPORTIVA", "FUN_BIKE", "HYPER_SPORT",
+				"MAXI_TRAIL", "MINIMOTOS", "NAKED", "OFF_ROAD",
+				"MOTOS_DE_TRIAL", "SCOOTERS", "SIDE_CAR", "STREET",
+				"STREETFIGHTER", "SUPERMOTARD", "UNDERBONE", "WHEELIE" };
+		String[] color = { "BRANCO", "INDIGO", "ROXO", "PINK", "AMARELO",
+				"CINZA", "MARROM", "PRETO", "PRATA", " VERMELHO", "LARANJA",
+				"AZUL", "DOURADO", "VERDE", "TURQUESA", "CASTANHO_AVERMELHADO" };
+		String[] Cam = { "CVT", "AUTOMATICO", "AUTOMATICO_SEQUENCIAL",
+				"SEMIAUTOMATICO", "MANUAL" };
+		String[] montCarro = { "BMW", "HONDA", "CHEVROLET", "FORD", "HYUNDAI",
+				"KIA", "NISSAN", "RENAULT", "TOYOTA", "VOLKSWAGEN" };
+		String[] tipoCar = { "HATCH", "SEDA", "MONOVOLUME", "PERUA", "PICAPE",
+				"SUV", "ESPORTIVO", "JIPE", "SCRAMBLER", "TRACKER", "BOBBER",
+				"CAFE_RACER", "CUSTOM", "CHOPPER" };
+		Cambio.add(Cam[0]);
+		Cambio.add(Cam[1]);
+		Cambio.add(Cam[2]);
+		Cambio.add(Cam[3]);
+		Cambio.add(Cam[4]);
+		Cor.add(color[0]);
+		Cor.add(color[1]);
+		Cor.add(color[2]);
+		Cor.add(color[3]);
+		Cor.add(color[4]);
+		Cor.add(color[5]);
+		Cor.add(color[6]);
+		Cor.add(color[7]);
+		Cor.add(color[8]);
+		Cor.add(color[9]);
+		Cor.add(color[10]);
+		Cor.add(color[11]);
+		Cor.add(color[12]);
+		Cor.add(color[13]);
+		Cor.add(color[14]);
+		Cor.add(color[15]);
+		Cor.contains(color);
+		montadoraMotocicleta.contains(montMotocicleta);
+		tipoMotocicleta.contains(tipoMoto);
+		tipoCarro.contains(tipoCar);
+		montadoraCarro.contains(montCarro);
+
+	}
+}
